5ec2579dc61ea0e288395104b6068d37eb915f78
AanPial <165553604+AanPial@users.noreply.github.com>
2024-04-04 13:43:22 +0600

    Add files via upload

---
+++ OOP/src/H1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class H1 {
+    public static void main(String []args)
+    {
+        Scanner p=new Scanner(System.in);
+        int x,y,z;
+        x=p.nextInt();
+        y=p.nextInt();
+        z=p.nextInt();
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
