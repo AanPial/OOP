408ba87d93810fd2fefc996ea6b510d45ab261b0
AanPial <165553604+AanPial@users.noreply.github.com>
2024-05-01 20:55:04 +0600

    Add files via upload

---
+++ GUI MAIN/withSwing/OptionPaneExample2.java
@@ -0,0 +1,11 @@
+package withSwing;
+import javax.swing.*;
+public class OptionPaneExample2 {
+    JFrame f=new JFrame();
+    public OptionPaneExample2(){
+        JOptionPane.showInputDialog(f,"Input");
+    }
+    public static void main(String[] args) {
+        new OptionPaneExample2();
+    }
+}
