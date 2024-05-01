6346cef73d1a2121b7ea3c33b7099680b516a21e
AanPial <165553604+AanPial@users.noreply.github.com>
2024-05-01 20:30:57 +0600

    Add files via upload

---
+++ GUI MAIN/withSwing/OptionPaneExample.java
@@ -0,0 +1,11 @@
+package withSwing;
+import javax.swing.*;
+public class OptionPaneExample {
+    JFrame f=new JFrame();
+    public OptionPaneExample(){
+        JOptionPane.showMessageDialog(f,"Hello, Welcome to Javatpoint");
+    }
+    public static void main(String[] args) {
+        new OptionPaneExample();
+    }
+}
